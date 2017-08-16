50c3ca92b90feeb15c1d10c34183ea3198318801
yuanpoint <2548131226@qq.com>
2017-08-16 10:24:35 +0800

    Update MipushController.class.php

--- Doc/MipushController.class.php
+++ Doc/MipushController.class.php
@@ -16,8 +16,8 @@ class MipushController extends MainController {
 	//Array ( [result] => ok [trace_id] => Xlm31b52487926945652nL [code] => 0 [data] => Array ( [id] => tlm31b52487926945784rQ ) [description] => 成功 [info] => Received push messages for 1 TOPIC )
 	//消息发送失败返回格式：
 	//Array ( [result] => error [reason] => Title or Description is empty! [trace_id] => Xlm15b74487927078175yr [code] => 65011 [description] => 未提供参数 )
-	private $secret='2paBxstzolDkcM4zaqYx7g==';//AppSecret
-	private $package='cn.iaapp.app.guanggaopai';//包名
+	private $secret='';//AppSecret
+	private $package='';//包名
 	
 	public function _initialize(){
 		vendor('MiPush.autoload');
